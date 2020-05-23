ed4fbf975933730196a125681a0b688a5db81937
dmitry_bandrovsky <ElDimon>
2020-05-23 11:55:12 +0200

    Initial Project Creation with Maven

---
+++ src/main/java/eld/springframework/sfddi/SfdDiApplication.java
@@ -0,0 +1,13 @@
+package eld.springframework.sfddi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfdDiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfdDiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eld/springframework/sfddi/SfdDiApplicationTests.java
@@ -0,0 +1,13 @@
+package eld.springframework.sfddi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfdDiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
